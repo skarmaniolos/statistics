41b460c55a5c0aaeb3769eef94c661541de7d3f6
Steven Karmaniolos <28944297+skarmaniolos@users.noreply.github.com>
2019-09-12 10:19:40 +1000

    added time off in $ feature and cleaned up UI

--- code/calculate_leave.py
+++ code/calculate_leave.py
@@ -12,14 +12,43 @@ def time_off(a_leave, p_leave):
 	print('  Days off  :  {}'.format(round(days_off,2)))
 	print('  Weeks off :  {}'.format(round(weeks_off,2)))
 	print(' =================== ')
+  print('        ...          ')
+  main()
+
+	
+def pay_calc(a_leave, p_leave, hourly_rate):
+  total_hours = a_leave + p_leave
+  total_pay = total_hours * hourly_rate
+  print(' =================== ')
+  print('  Total Hours:  {}').format(round(total_hours,2))
+  print('  Total Pay  :  {}').format(round(total_pay,2))
+  print(' =================== ')
+  print('        ...          ')
+  main()
 
 
-@click.command()
+def menu(option):
+	if option == 1:
+		a_leave = input('Enter your annual leave hours: ')
+		p_leave = input('Enter your personal leave hours: ')
+		time_off(a_leave, p_leave)
+	elif option == 2:
+		a_leave = input('Enter your annual leave hours: ')
+		p_leave = input('Enter your personal leave hours: ')
+		hourly_rate = input('Enter your hourly pay rate: ')
+		pay_calc(a_leave, p_leave, hourly_rate)
+	elif option == 0:
+		exit()
+		
+
 def main():
-	a_leave = float(input('Enter your annual leave hours: '))
-	p_leave = float(input('Enter your personal leave hours: '))
-	time_off(a_leave, p_leave)
+	print(' =================== ')
+	print('  Menu:')
+	print('  1 : Time Off (Time)')
+	print('  2 : Time Off ($)')
+	print('  0 : Exit')
+	print(' =================== ')
+	option = input('>')
+	menu(option)
 
-
-if __name__ == "__main__":
-	main()
+main()
