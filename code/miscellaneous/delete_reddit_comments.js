8318e63a64535c1d6335500b5d0faf72244b9068
dietofworms <28944297+dietofworms@users.noreply.github.com>
2020-08-19 16:43:55 +1000

    added code for deleting reddit comments

---
+++ code/miscellaneous/delete_reddit_comments.js
@@ -0,0 +1,12 @@
+var $domNodeToIterateOver = $('.del-button .option .yes'),
+            currentTime = 0,
+            timeInterval = 1500;
+
+$domNodeToIterateOver.each(function() {
+          var _this = $(this);
+          currentTime = currentTime + timeInterval;
+
+          setTimeout(function() {
+                      _this.click();
+                    }, currentTime);
+});
